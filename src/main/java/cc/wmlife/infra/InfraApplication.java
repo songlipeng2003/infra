d6ae8082606b0f4e3d909dd3f0f2f290884a933c
Thinking Song <123wuming_123@163.com>
2022-09-15 17:22:34 +0800

    init project

---
+++ src/main/java/cc/wmlife/infra/InfraApplication.java
@@ -0,0 +1,12 @@
+package cc.wmlife.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfraApplication.class, args);
+    }
+}
